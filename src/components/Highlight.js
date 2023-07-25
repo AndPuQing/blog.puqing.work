07dee0561cc91b710f3408be6e5c6158d380a89e
PuQing <me@puqing.work>
2023-07-25 21:07:06 +0800

    fix color

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useColorMode } from "@docusaurus/theme-common";
 
 export default function Highlight({ children, color }) {
+    const isDarkTheme = useColorMode().colorMode === 'dark';
     color = color ? color : '#ffec99';
+    if (isDarkTheme) {
+        color = '#ff9c00';
+    }
     return (
         <span
             style={{
