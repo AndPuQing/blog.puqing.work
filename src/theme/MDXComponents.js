90084f97b8eb8424c88849b7b20f072b0c6a9ee3
PuQing <me@puqing.work>
2023-08-22 22:52:55 +0800

    add Geogebra

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,7 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import Highlight from '@site/src/components/Highlight';
+import Geogebra from '@site/src/components/Geogebra';
 
 export default {
     // Re-use the default mapping
@@ -9,4 +10,6 @@ export default {
     // Map the "<Highlight>" tag to our Highlight component
     // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
     Highlight,
+    // Map the "<Geogebra>" tag to our Geogebra component
+    Geogebra,
 };
